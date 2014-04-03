c02a565062f0e72e80d53ec830479927f42382fe
Andres Lamont <alamont@gmail.com>
2014-04-03 16:59:34 +0200

    Nice our team graphics

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -9,7 +9,7 @@ Template.home.rendered = function (){
 	$('.s5').attr('data-stellar-background-ratio', 0.8);
 	$('.s5').attr('data-stellar-vertical-offset', -400);
 	$('.s7').attr('data-stellar-background-ratio', 0.8);
-	$('.s7').attr('data-stellar-vertical-offset', -400);
+	$('.s7').attr('data-stellar-vertical-offset', -600);
   $.stellar();
   $(document).foundation();
   leaflet();
