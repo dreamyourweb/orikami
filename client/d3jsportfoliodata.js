bcd5d05e5ede53084a94fcda3ebbe3fe252c62ae
Hanneke van den Akker <hanneke@orikami.nl>
2014-11-11 12:30:04 +0100

    Added picture of Niklas and SOTP and YBD to portfolio.

--- client/d3jsportfoliodata.js
+++ client/d3jsportfoliodata.js
@@ -1,7 +1,7 @@
 PortfolioData = [
     {
         img: "images/umap.png",
-        name: "umap",
+        name: "U-map",
         techniques: [
             "Ruby on Rails",
             "SVG",
@@ -13,9 +13,9 @@ PortfolioData = [
         data_product: "Together with the Center for Higher Educational Policy Studies we map universities worldwide on 29 dimensions (read: not just scientific output!). The multi-dimensional classification is gathered from universities. Source information is rigorlously checked and standardized over countries and measurement standards. The mapping gives rise to unforeseen connections and similarities between universities. A simple, but efficient algorithm calculates similarity between universities.",
         website: "http://www.u-map.org",
         hours: 50,
-        x: 18,
+        x: 15,
         y: 15,
-        r: 10,
+        r: 9,
         color: "#29abe2",
         index: 1
     },
@@ -35,11 +35,10 @@ PortfolioData = [
         data_product: "Mozart we develop in deep collaboration with market research company DVJ insights. Mozart listens to open sources like Twitter, Facebook, YouTube and Last.FM to learn what people are saying. We combine this with specialized information from radio airplay, spotify and youtube. The last piece of information comes from a large user panel. We sample feedback from users about what they like.",
         intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
         insights: "The multidimensional classification is presented in an interactive and simple way.",
-        website: "www.u-map.org",
         hours: 1000,
-        x: 40,
+        x: 36,
         y: 10,
-        r: 7,
+        r: 6.5,
         color: "rgba(65, 0, 80, 1)",
         index: 2
     },
@@ -57,12 +56,12 @@ PortfolioData = [
             "Collaborative Filters",
             "Bayesian classifiers"
         ],
-        data_product: "Discover your favorite beer, know where this beer is served and build your personal beer journal; This is the BierApp. It gives you advice on what to drink next and shows the beer listings of café’s. The 'BierApp' makes sure you always drink the right beer on the right occasion.",
+        data_product: "Discover your favorite beer, know where this beer is served and build your personal beer journal; This is the BierApp. It gives you advice on what to drink next and shows the beer listings of café’s. The 'BierApp' makes sure you always drink the right beer on the right occasion.      ",
         intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
         insights: "The multidimensional classification is presented in an interactive and simple way.",
-        website: "www.u-map.org",
+        website: "http://www.debierapp.nl/",
         hours: 5000,
-        x: 60,
+        x: 50,
         y: 25,
         r: 8,
         y_mid: 100,
@@ -70,26 +69,65 @@ PortfolioData = [
         index: 3
     },
         {
-        img: "images/kplusv.png",
-        name: "KplusV",
+        img: "images/sotp.png",
+        name: ("Smell of the place"),
         techniques: [
-            "Ruby on Rails",
-            "Javascript",
+            "meteor.js",
             "d3.js",
-            "Linear Algebra",
-            "MongoDB",
-            "Data Visualisation"
+            "data visualisation",
+            "bench marking"
         ],
-        data_product: "In co-creation with KplusV we developed a web application, which maps the vitality of a cultural organisation. By means of a questionnaire we measure innovativity, distinctiveness, strategy and business execution. When the questionnaire is finished, the application presents a diagnosis.",
+        data_product: "How do people perceive their workplace? Do they enjoy and feel supported or are they under pressure and don't feel autonomous? This data in most companies is collected yearly through extensive, non-actionable surveys. In cooperation with Radboudumc we are measuring the daily 'smell of the place'. If they wish, employees can complete an attractive survey daily. Once it is filled out, employees view their results and compare them to others. In addition, trend graphs can be shown for the whole organisation, or segmented into sections based on age, gender or department. Daily actionable data collection about the experiences of employees is what Smell of the place offers. Do you want to know the 'smell' of your department? ",
         intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
         insights: "The multidimensional classification is presented in an interactive and simple way.",
-        website: "www.u-map.org",
         hours: 5000,
-        x: 80,
-        y: 20,
-        r: 5,
+        x: 70,
+        y: 13,
+        r: 9.3,
         y_mid: 100,
         color: "rgba(147, 185, 75, 1)",
         index: 4
-    }
+    },
+        {
+        img: "images/youbedo.png",
+        name: "YouBeDo",
+        techniques: [
+            "Elastic Search",
+            "node.js"
+        ],
+        data_product: "YouBeDo wants to do good. They achieve this by giving 10% of your purchase to a charity of your choice. Their online bookstore offers over two million books for sale. We help them search this vast amount of book titles. We developed the search engine behind YouBeDo.com and are analyzing its performance on conversion. After optimization YouBeDo and orikami will help you find books like never before.",
+        intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
+        insights: "The multidimensional classification is presented in an interactive and simple way.",
+        website: "http://www.youbedo.com/",
+        hours: 5000,
+        x: 87,
+        y: 28,
+        r: 6,
+        y_mid: 100,
+        color: "rgba(255, 153, 0, 1)",
+        index: 5
+    },
+//        {
+//        img: "images/kplusv.png",
+//        name: "KplusV",
+//        techniques: [
+//            "Ruby on Rails",
+//            "Javascript",
+//            "d3.js",
+//            "Linear Algebra",
+//            "MongoDB",
+//            "Data Visualisation"
+//        ],
+//        data_product: "In co-creation with KplusV we developed a web application, which maps the vitality of a cultural organisation. By means of a questionnaire we measure innovativity, distinctiveness, strategy and business execution. When the questionnaire is finished, the application presents a diagnosis.",
+//        intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
+//        insights: "The multidimensional classification is presented in an interactive and simple way.",
+//        website: "http://www.cultureelondernemenscan.nl/",
+//        hours: 5000,
+//        x: 90,
+//        y: 20,
+//        r: 4,
+//        y_mid: 100,
+//        color: "rgba(22, 115, 143, 1)",
+//        index: 6
+//    },
 ]
