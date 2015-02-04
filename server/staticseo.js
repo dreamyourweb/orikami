510aaf8f96cbff9a3bdb90d81a55ea924b64a75f
Hanneke van den Akker <hanneke@orikami.nl>
2015-02-04 17:08:38 +0100

    seo information for specific landing page

---
+++ server/staticseo.js
@@ -0,0 +1,22 @@
+Meteor.startup(function() {
+    SeoCollection.update(
+    {
+        route_name: 'Data visualisatie'
+    },
+    {
+        $set: {
+            route_name: 'data-visualisatie',
+            title: 'Data visualisatie - Door inzicht naar de juiste bedrijfsbeslissing',
+            meta: {
+                'description': 'Maatwerk oplossingen om informatie uit uw data te halen en deze overzichtelijk te presenteren.'
+            },
+            og: {
+                'title': 'Data visualisaties',
+                }
+        }
+    },
+    {
+        upsert: true
+    }
+  );
+});  // als je niet precies weet wat de route_name is, kan je in de console (bij element inspecteren) typen: 'Router.current().route.getName()'
