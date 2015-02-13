c196bf3ea240a1bf0d32e0acb8a0a6ce485828e9
Hanneke van den Akker <hanneke@orikami.nl>
2015-02-13 12:17:36 +0100

    adding three landing pages

--- server/staticseo.js
+++ server/staticseo.js
@@ -66,3 +66,73 @@ Meteor.startup(function() {
     }
   );
 });
+
+Meteor.startup(function() {
+    SeoCollection.update(
+    {
+        route_name: 'lean-management'
+    },
+    {
+        $set: {
+            route_name: 'lean-management',
+            title: 'Lean management - Een universele strategie om snel en efficiënt goede resultaten te behalen bij een innovatief project',
+            meta: {
+                'description': 'Lean management is een systematische manier van processen begeleiden en coördineren, zodat optimaal gebruik wordt gemaakt van resources en capaciteiten.'
+            },
+            og: {
+                'title': 'Lean management',
+                }
+        }
+    },
+    {
+        upsert: true
+    }
+  );
+});
+
+Meteor.startup(function() {
+    SeoCollection.update(
+    {
+        route_name: 'machine-learning'
+    },
+    {
+        $set: {
+            route_name: 'machine-learning',
+            title: 'Machine learning - Geautomatiseerd leren van data',
+            meta: {
+                'description': 'De modernste technieken om geautomatiseerd te leren van data, en informatie waardevol te maken voor alle bedrijfsniveaus.'
+            },
+            og: {
+                'title': 'Machine learning',
+                }
+        }
+    },
+    {
+        upsert: true
+    }
+  );
+});
+
+Meteor.startup(function() {
+    SeoCollection.update(
+    {
+        route_name: 'Ruby-on-rails'
+    },
+    {
+        $set: {
+            route_name: 'Ruby-on-rails',
+            title: 'Ruby on Rails. Efficiente, flexibele en schaalbare webapplicaties met Ruby on Rails"',
+            meta: {
+                'description': 'Efficiënte, flexibele en schaalbare webapplicaties met Ruby on Rails.'
+            },
+            og: {
+                'title': 'Ruby on rails',
+                }
+        }
+    },
+    {
+        upsert: true
+    }
+  );
+});
+
