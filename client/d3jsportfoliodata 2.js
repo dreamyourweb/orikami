cd45cba44f46b4bad50616e55c5bae5f68f8fba3
Hanneke van den Akker <hanneke@orikami.nl>
2015-03-19 12:09:19 +0100

    portfolio update, arrows and screenshots umap

--- client/d3jsportfoliodata 2.js
+++ client/d3jsportfoliodata 2.js
@@ -1,11 +1,11 @@
-
 PortfolioData = [
     {
         img: "images/umap.png",
-        name: "U-Map",
-        img2: "images/umap2.png",
-        img3: "images/umap3.png",
-        img4: "images/umap4.png",
+        name: "U-map",
+        img2: "images/umap7.png",
+        img3: "images/umap5.png",
+        img4: "images/umap6.png",
+        img5: "images/umap3.png",
           techniques: [
             "Ruby on Rails",
             "SVG",
@@ -14,12 +14,12 @@ PortfolioData = [
             "MongoDB",
             "Data Visualisation"
         ],
-        data_product: "Together with the Center for Higher Educational Policy Studies we map universities worldwide on 29 dimensions (read: not just scientific output!). This multi-dimensional classification is gathered from universities. Source information is rigorlously checked and standardized over countries and measurement standards. The mapping unmasks unforeseen connections and similarities between universities. A simple, but efficient algorithm calculates similarity between universities. \r\n",
+        data_product: "Together with the Center for Higher Educational Policy Studies we map universities worldwide on 29 dimensions (read: not just scientific output!). The multi-dimensional classification is gathered from universities. Source information is rigorlously checked and standardized over countries and measurement standards. The mapping gives rise to unforeseen connections and similarities between universities. A simple, but efficient algorithm calculates similarity between universities. \r\n",
         umap: "http://www.u-map.org",
         hours: 50,
-        x: 15,
-        y: 15,
-        r: 9,
+        x: 10,
+        y: 25,
+        r: 7,
         color: "#29abe2",
         index: 1,
 
@@ -39,13 +39,13 @@ PortfolioData = [
             "Bayesian classifiers",
             "Lucene"
         ],
-        data_product: "Mozart is developed in deep collaboration with market research company DVJ Insights. Mozart listens to open sources such as Twitter, Facebook, YouTube and Last.FM to learn what people are saying about music. We combine this with specific information from radio airplay, Spotify and Youtube. The final piece of the puzzle comes from a large user panel. We sample feedback from users about what they like.",
+        data_product: "Mozart we develop in deep collaboration with market research company DVJ insights. Mozart listens to open sources like Twitter, Facebook, YouTube and Last.FM to learn what people are saying. We combine this with specialized information from radio airplay, spotify and youtube. The last piece of information comes from a large user panel. We sample feedback from users about what they like.",
         intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
         insights: "The multidimensional classification is presented in an interactive and simple way.",
         hours: 1000,
-        x: 36,
-        y: 10,
-        r: 6.5,
+        x: 30,
+        y: 15,
+        r: 10,
         color: "rgba(65, 0, 80, 1)",
         index: 2
     },
@@ -67,14 +67,14 @@ PortfolioData = [
             "Collaborative Filters",
             "Bayesian classifiers"
         ],
-        data_product: "Discover your favorite beer, find out where this beer is served and build your personal beer journal. This is the BierApp. It gives you advice on what to drink next and shows the beer listings of cafes. The 'BierApp' makes sure you always drink the right beer on the right occasion. \n",
+        data_product: "Discover your favorite beer, know where this beer is served and build your personal beer journal; This is the BierApp. It gives you advice on what to drink next and shows the beer listings of cafes. The 'BierApp' makes sure you always drink the right beer on the right occasion. \n",
         intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
         insights: "The multidimensional classification is presented in an interactive and simple way.",
         Bierapp: "http://www.debierapp.nl/",
         hours: 5000,
-        x: 50,
-        y: 25,
-        r: 8,
+        x: 45,
+        y: 28,
+        r: 6,
         y_mid: 100,
         color: "rgba(156, 38, 21, 1)",
         index: 3
@@ -93,11 +93,11 @@ PortfolioData = [
             "data visualisation",
             "bench marking"
         ],
-        data_product: "How do people perceive their workplace? Do they enjoy and feel supported or are they under pressure and don't feel autonomous? In most companies this data is collected annually through extensive, non-actionable surveys. In cooperation with Radboudumc we are measuring the daily 'smell of the place'. With an intuitive app employees can daily fill out an attractive survey. Employees can view their results and compare them with their colleagues. In addition, trend graphs can be generated for the whole organisation, or segmented into sections based on age, gender or department. Daily actionable data collection about the experiences of employees is what Smell of the place offers. Do you want to know the 'smell' of your department? ",
+        data_product: "How do people perceive their workplace? Do they enjoy and feel supported or are they under pressure and don't feel autonomous? This data in most companies is collected yearly through extensive, non-actionable surveys. In cooperation with Radboudumc we are measuring the daily 'smell of the place'. If they wish, employees can complete an attractive survey daily. Once it is filled out, employees view their results and compare them to others. In addition, trend graphs can be shown for the whole organisation, or segmented into sections based on age, gender or department. Daily actionable data collection about the experiences of employees is what Smell of the place offers. Do you want to know the 'smell' of your department? ",
         intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
         insights: "The multidimensional classification is presented in an interactive and simple way.",
         hours: 5000,
-        x: 70,
+        x: 59,
         y: 13,
         r: 9.3,
         y_mid: 100,
@@ -118,13 +118,59 @@ PortfolioData = [
         insights: "The multidimensional classification is presented in an interactive and simple way.",
         YouBeDo: "http://www.youbedo.com/",
         hours: 5000,
-        x: 87,
-        y: 28,
+        x: 70,
+        y: 29,
         r: 6,
         y_mid: 100,
         color: "rgba(255, 153, 0, 1)",
         index: 5
     },
+    /*
+          {
+        img: "images/youbedo.png",
+        name: "Connecting Mobility",
+        img2: "images/",
+        img3: "images/",
+        techniques: [
+            "Meteor",
+            "MongoDB",
+            "d3.js"
+           
+        ],
+        data_product: "Connecting Mobility bla bla ",
+        intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
+        insights: "The multidimensional classification is presented in an interactive and simple way.",
+        ConnectingMobility: "http://itsoverzicht.connectingmobility.nl/",
+        hours: 5000,
+        x: 85,
+        y: 12,
+        r: 9.6,
+        y_mid: 100,
+        color: "rgba(205, 197, 191, 1)",
+        index: 6
+    },
+          {
+        img: "images/youbedo.png",
+        name: "IRMA",
+        img2: "images/youbedo2.png",
+        img3: "images/youbedo1.png",
+        techniques: [
+            "Angular", 
+            "Firebase",
+            "d3.js"
+        ],
+        data_product: "YouBeDo wants to do good. They achieve this by giving 10% of your purchase to a charity of your choice. Their online bookstore offers over two million books for sale. We help them search this vast amount of book titles. We developed the search engine behind YouBeDo.com and are analyzing its performance on conversion. After optimization YouBeDo and orikami will help you find books like never before.",
+        intelligent: "There is a lot of information In the 29 dimensional data of all universities. This data encloses connections between uni-versities never seen before. We devel-oped a simple but efficient algorithm to discover and compare universities based on similarity.",
+        insights: "The multidimensional classification is presented in an interactive and simple way.",
+        IRMA: "http://www.youbedo.com/",
+        hours: 5000,
+        x: 95,
+        y: 31,
+        r: 5,
+        y_mid: 100,
+        color: "rgba(255, 215, 0, 1)",
+        index: 7
+    }, */
 //        {
 //        img: "images/kplusv.png",
 //        name: "KplusV",
@@ -148,5 +194,4 @@ PortfolioData = [
 //        color: "rgba(22, 115, 143, 1)",
 //        index: 6
 //    },
-]
-
+];
